e264659bc6a6b4d90afc979c2c91f1f117a49ca8
ThanhNguyxn <thanhnguyentuan2007@gmail.com>
2025-12-22 13:04:52 +0700

    feat(core): add configurable shortcut system
    - Add shortcut.rs with ShortcutTable (enable/disable, add/remove)
    - Default shortcuts: vn, dc, ko, hcm, hn (immediate trigger)
    - Update engine.rs to use new shortcut API
    - Remove old shortcuts.rs
    - Fix .gitignore to exclude **/target/

--- core/target/debug/deps/vietflux_core-8b72f0d44bfffef3.d
+++ core/target/debug/deps/vietflux_core-8b72f0d44bfffef3.d
@@ -1,6 +1,6 @@
-D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core-8b72f0d44bfffef3.d: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcuts.rs src\transform.rs src\validation.rs
+D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core-8b72f0d44bfffef3.d: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcut.rs src\transform.rs src\validation.rs
 
-D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core-8b72f0d44bfffef3.exe: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcuts.rs src\transform.rs src\validation.rs
+D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core-8b72f0d44bfffef3.exe: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcut.rs src\transform.rs src\validation.rs
 
 src\lib.rs:
 src\buffer.rs:
@@ -9,6 +9,6 @@ src\engine.rs:
 src\methods\mod.rs:
 src\methods\telex.rs:
 src\methods\vni.rs:
-src\shortcuts.rs:
+src\shortcut.rs:
 src\transform.rs:
 src\validation.rs:
--- core/target/debug/deps/vietflux_core.d
+++ core/target/debug/deps/vietflux_core.d
@@ -1,8 +1,8 @@
-D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core.d: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcuts.rs src\transform.rs src\validation.rs
+D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core.d: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcut.rs src\transform.rs src\validation.rs
 
-D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core.dll: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcuts.rs src\transform.rs src\validation.rs
+D:\Code\vietflux-ime\core\target\debug\deps\vietflux_core.dll: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcut.rs src\transform.rs src\validation.rs
 
-D:\Code\vietflux-ime\core\target\debug\deps\libvietflux_core.rlib: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcuts.rs src\transform.rs src\validation.rs
+D:\Code\vietflux-ime\core\target\debug\deps\libvietflux_core.rlib: src\lib.rs src\buffer.rs src\chars.rs src\engine.rs src\methods\mod.rs src\methods\telex.rs src\methods\vni.rs src\shortcut.rs src\transform.rs src\validation.rs
 
 src\lib.rs:
 src\buffer.rs:
@@ -11,6 +11,6 @@ src\engine.rs:
 src\methods\mod.rs:
 src\methods\telex.rs:
 src\methods\vni.rs:
-src\shortcuts.rs:
+src\shortcut.rs:
 src\transform.rs:
 src\validation.rs:
